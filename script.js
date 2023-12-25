12d45f6c713e51209c2ea8f07e99f3df12e6b744
Chukwuebuka Alaegbu <chukwuebukaalaegbu@gmail.com>
2023-12-25 17:30:33 +0100

    Added attack function

--- script.js
+++ script.js
@@ -107,7 +107,22 @@ function goFight(n) {
     monsterName.innerText = monsters[n].name;
     monsterHealthText.innerText = monsterHealth;
 }
-
+function attack() {
+    text.innerText = "The " + monsters[fighting].name + " attacks.";
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name + ".";
+    health -= getMonsterAttackValue(monsters[fighting].level);
+    if(isMonsterHit()){
+  
+    }
+    monsterHealth -= weapons[currentWeapon].power + Math.floor(Math.random() * xp) + 1;
+    healthText.innerText = health;
+    monsterHealthText.innerText = monsterHealth;
+    if (health <= 0) {
+      lose();
+    } else if (monsterHealth <= 0) {
+      fighting === 2 ? winGame() : defeatMonster();
+    }
+  }
 function defeatMonster() {
     gold += Math.floor(monsters[fighting].level * 6.7);
     xp += monsters[fighting].level;
